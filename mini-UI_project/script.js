163c4ec76ce2d75d8cd0beb3f4fd9993d752b7f9
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2024-03-15 20:39:25 +0530

    second

--- mini-UI_project/script.js
+++ mini-UI_project/script.js
@@ -1,9 +1,15 @@
-﻿const nav = document.querySelector('.robot-hand')
+﻿const nav = document.querySelector(".robot-hand");
+const SelectOption = document.getElementById("pay_methods");
+const btn = document.getElementById("selector-arrow");
 
- 
 addEventListener("scroll", () => {
   let value = window.scrollY;
-  console.log(value)
-   nav.style.left= value+270 + "px";
-
+  console.log(value);
+  let width = window.innerWidth;
+  if (width < 1440) {
+    nav.style.left = value + 270 + "px";
+  }
 });
+const toggle=()=>{
+SelectOption.classList.toggle('active')
+}
