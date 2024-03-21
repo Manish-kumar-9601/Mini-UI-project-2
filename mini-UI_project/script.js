3d9b368cf048d3bbb3d372b1d6fd7f6bb0213717
Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>
2024-03-21 12:06:20 +0530

    menu button function is done
    Signed-off-by: Manish kumar <134037752+Manish-kumar-9601@users.noreply.github.com>

--- mini-UI_project/script.js
+++ mini-UI_project/script.js
@@ -1,15 +1,40 @@
-﻿const nav = document.querySelector(".robot-hand");
+﻿const robotHand = document.querySelector(".robot-hand");
 const SelectOption = document.getElementById("pay_methods");
 const btn = document.getElementById("selector-arrow");
+const menuIcon = document.getElementsByClassName("menu-icon")[0];
+const menuImg = document.getElementById("menu-icon-img");
+const menuArea = document.querySelector(".nav_link");
+const link_btn = document.querySelectorAll(".nav_link a");
+menuArea.style.padding = "0";
+menuArea.style.border = "none";
+// let screenWidth = window.innerWidth;
 
-addEventListener("scroll", () => {
-  let value = window.scrollY;
-  console.log(value);
-  let width = window.innerWidth;
-  if (width < 1440) {
-    nav.style.left = value + 270 + "px";
+let e;
+menuIcon.addEventListener("click", () => {
+  let toggle;
+  console.log(menuIcon);
+  e = !e;
+  console.log(e, "toggle");
+
+  if (e) {
+    menuImg.src = "icons8-x-64.png";
+    menuArea.style.padding = "10px";
+    menuArea.style.border = "2px";
+    toggle = "block";
+  } else {
+    menuImg.src = "icons8-menu-48.png";
+    toggle = "none";
+  }
+
+  for (let i = 0; i <= 3; i++) {
+    document.querySelectorAll(".link-btn")[i].style.display = toggle;
   }
 });
-const toggle=()=>{
-SelectOption.classList.toggle('active')
-}
+// addEventListener("scroll", () => {
+//   let value = window.scrollY;
+//   console.log(value);
+//   let width = window.innerWidth;
+//   if (width < 1440) {
+//     robotHand.style.left = value + 270 + "px";
+//   }
+// });
